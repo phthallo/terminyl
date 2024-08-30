edaa0f6e97a59fa7403e33e4a85925db33f0630c
phthallo <annabelquach13@gmail.com>
2024-08-30 17:46:35 +0930

    fixing stuff!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # terminyl
- A CLI Pomodoro Timer in Rust.
+ A CLI Pomodoro Timer in Rust <sub>but because I don't really know Rust, it's really bad</sub>
+
+To use, clone the repository and `cd terminyl`. 
+Then run `cargo run <study_time> <rest_time> <session count>` in a terminal window.
+
--- terminyl/src/main.rs
+++ terminyl/src/main.rs
@@ -7,21 +7,24 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let study: usize = (&args[1]).parse().unwrap();
     let rest: usize = (&args[2]).parse().unwrap();
-    println!("Starting timer for {study} minutes of study.\n");
-    countdown(study);
-    println!("Starting break for {rest} minutes of rest! Great work, you got this! \n");
-    countdown(rest);
+    let session_number: usize = (&args[3]).parse().unwrap();
+    for i in 0..session_number {
+        println!("\nSession {}: Starting timer for {} minutes of study.\n", i+1, study);
+        countdown(study);
+        println!("\nStarting break for {} minutes of rest! Great work, you got this! \n", rest);
+        countdown(rest);
+    }
 }
 
 fn countdown(time: usize) {
     let minutes = Duration::new(1, 0);
-    for i in 0..time {
+    for i in 0..time+1 {
         let remaining_time : usize = time-i;
-        let thing : usize = (((remaining_time as f64/time as f64)*100.0)/10.0) as usize;
-        let thing2 : &str = &"|".repeat(thing);
-        print!("{remaining_time} minutes remaining! {}\r", thing2);
+        let thing : usize = ((((remaining_time as f64/time as f64)*100.0)/10.0)) as usize;
+        print!("\r     > {} minutes remaining!      {:>10}", remaining_time, "█".repeat(thing));
         let _ = std::io::stdout().flush();
         thread::sleep(minutes);
     }
+    print!("\n");
 
 }
